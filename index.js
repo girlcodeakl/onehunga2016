4a9ddb1ec53c96fd81bdcb703ac050c97e836f8f
oreo-queen <tq2097@onelearn.nz>
2016-09-13 16:47:04 +1200

    Heroku

--- index.js
+++ index.js
@@ -35,5 +35,5 @@ app.post('/ideas', saveNewIdea);
 app.post('/ideas', )
 
 //listen for connections on port 3000
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log("I am listening...");
